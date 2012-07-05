a1603cd6e84f870fcfb89e446be540091bf0a8bb
Radoslav Asparuhov <rasparuhov@gmail.com>
2012-07-05 18:51:53 +0300

    no idea how to make a request

--- SimpleFace/src/com/example/simpleface/MainActivity.java
+++ SimpleFace/src/com/example/simpleface/MainActivity.java
@@ -2,28 +2,55 @@ package com.example.simpleface;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import com.facebook.android.*;
 import com.facebook.android.Facebook.*;
+//import com.facebook.android.RequestListener;
 
 public class MainActivity extends Activity {
 
-    Facebook facebook = new Facebook("56b3e3b8");
+    Facebook facebook = new Facebook("faM0RBE10tnx+tvv9TCzTQ3CKPU");
+    private SharedPreferences mPrefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+        /*
+         * Get existing access_token if any
+         */
+        mPrefs = getPreferences(MODE_PRIVATE);
+        String access_token = mPrefs.getString("access_token", null);
+        long expires = mPrefs.getLong("access_expires", 0);
+        if(access_token != null) {
+            facebook.setAccessToken(access_token);
+        }
+        if(expires != 0) {
+            facebook.setAccessExpires(expires);
+            //facebook.request("me/albums", new Bundle() );
+        }
+        
+        /*
+         * Only call authorize if the access_token has expired.
+         */
+        if(!facebook.isSessionValid()) {
 
-        facebook.authorize(this, new DialogListener() {
-            public void onComplete(Bundle values) {}
-
-            public void onFacebookError(FacebookError error) {}
-
-            public void onError(DialogError e) {}
-
-            public void onCancel() {}
-        });
+            facebook.authorize(this, new String[] {}, new DialogListener() {
+                public void onComplete(Bundle values) {
+                    SharedPreferences.Editor editor = mPrefs.edit();
+                    editor.putString("access_token", facebook.getAccessToken());
+                    editor.putLong("access_expires", facebook.getAccessExpires());
+                    editor.commit();
+                }
+    
+                public void onFacebookError(FacebookError error) {}
+    
+                public void onError(DialogError e) {}
+    
+                public void onCancel() {}
+            });
+        }
     }
 
     @Override
@@ -31,5 +58,10 @@ public class MainActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
 
         facebook.authorizeCallback(requestCode, resultCode, data);
+    }
+    
+    public void onResume() {    
+        super.onResume();
+        facebook.extendAccessTokenIfNeeded(this, null);
     }
 }
